69e0a2134a57c29634083704b835ce226c035f47
muazkhn <muaazkhns@gmail.com>
2026-01-02 09:05:16 -0800

    assignment

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+ Q:1
+ alert("Hello")
+
+
+
+ Q:2
+ alert("Error! Please Enter a valid password")
+
+
+ Q:3
+ var str1 = "Hello"
+ var str2 = "welcome to my web"
+
+ document.write(str1 + "<br>" + str2);
+
+
+ Q:4
+ alert("welcome to js land");
+ alert("happty coding");
+
+
+ Q:5
+ var value = "hello... i can run JS Through my web browser console";
+ alert(value);
+ console.log(value);
+
+
+ Q:6
+ alert ("Hello user i am syed azam bukhari and i am developer")
+
+
+ 
